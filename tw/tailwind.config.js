51b020807fe9aa14dac3240f1584e11ad15a2699
songryu <108215485+songryu@users.noreply.github.com>
2024-01-24 16:30:02 +0900

    committ

---
+++ tw/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+// const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  darkMode: 'class',
+  theme: {
+    // container: {
+    //   center: true,
+    // },
+
+    extend: {
+      colors: {
+        'my-color': '#0000ff',
+      },
+    },
+  },
+  plugins: [],
+};
