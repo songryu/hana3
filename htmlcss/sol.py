dff97635b54c093253aba90d5ff34613b1e5c5e3
songryu <108215485+songryu@users.noreply.github.com>
2024-01-30 10:19:29 +0900

    Object & Property exam

--- htmlcss/sol.py
+++ htmlcss/sol.py
@@ -1,14 +1,40 @@
 import sys
-import math
+import heapq
 input=sys.stdin.readline
-t=int(input())
+
 n,m=map(int,input().split())
 j=int(input())
 k=int(input())
-for _ in range(k):
-    ahouse=list(map(int,input().split()))
-for _ in range(k):
-    bhouse=list(map(int,input().split())
+INF=int(1e9)
+distance=[INF]*(n+1)
+ahouse=list(map(int,input().split()))
+bhouse=list(map(int,input().split()))
+            
+graph=[[] for _ in range(n+1)]
+
 for _ in range(m):
     x,y,z=map(int,input().split())
-    
+    graph[x].append((z,y))
+    graph[y].append((z,x))
+
+def diks(start):
+    q=[]
+    print(distance)
+    heapq.heappush(q,(0,start)) #cost
+    distance[start]=0
+    while q:
+        dist,node=heapq.heappop(q)
+        if distance[node]<dist:
+                continue
+        for cost,n in graph[node]:
+             costed=dist+cost
+             if costed<distance[n]:
+                  distance[n]=costed
+                  heapq.heappush(q,(costed,n))
+    return distance
+
+dis=diks(2)
+print(dis)
+# for i in range(1,n+1):
+#     dis=diks(i)
+#     print(dis[2])
--- js/obiprop1.js
+++ js/obiprop1.js
@@ -1,57 +1,57 @@
 const user = {
-    '': 1,        
+    '': 1,
     ' ': 1,       // 'id': 1, '0y': 2 모두 OK!
     123: 1,       // user[123], user['123'] OK, but user.123 is SyntaxError!!   // user[12345], user[12345n], user['12345'] OK, but user['12345n'] is undefined!
     true: 1,      // OK  user[true]  user.true
-    id: 2,          
+    id: 2,
     [`name`]: 'Hong',  // But, `name`: 'Hong'은 SyntaxError: Unexpected template string!
     [Symbol()]: 'Hong',   // OK But, Symbol(): 'Hong'은 SyntaxError: Unexpected token ':'
     [`${new Date()}`]: 365,    // OK! 'Sun Jul …': 365
     'my-friends': ['Han', 'Kim'],
     getInfo: () => `${this.id}-${this.name}`,       // OK! But, this is not user!
     getInfo() { return `${this.id}-${this.name}`; }, // OK! getInfo의 최종 <f.o>
-  }
-  console.log("---------");
-  console.log(Object.keys(user), Object.keys(user).length); // keys & 15, Symbol은 제외!!
+}
+console.log("---------");
+console.log(Object.keys(user), Object.keys(user).length); // keys & 15, Symbol은 제외!!
 
-  console.log(Reflect.ownKeys(user), Reflect.ownKeys(user).length); // keys & 16 (+Symbol)
-  
-  user.addr = 'Seoul';   // ⇐⇒ user = {...user, addr: 'Seoul'}
-  console.log('addr' in user, user.hasOwnProperty('addr')); // true true
-  console.log('Ref.has>', Reflect.has(user, 'addr'));  // true
-  console.log('obj.getOwnPropSym>', Object.getOwnPropertySymbols(user)); // [ Symbol() ]
-  
-  
-  delete user.addr;    // ⇔ Reflect.deleteProperty(user, 'addr');
-  console.log('addr' in user); // false
-  
-  
-  user[`${user.id}'s name`] = `Mr. ${user.name}`;     // prop생성시 snapshot!!(id 변해도 고정)
-  console.log('user entries=', Object.entries(user)); // Symbol은 제외!!
-  
-  function myEntries(obj){
-    const rets=[]; //[[k,v],[k,v],...]
-    for (let k of Reflect.ownKeys(obj)){
+console.log(Reflect.ownKeys(user), Reflect.ownKeys(user).length); // keys & 16 (+Symbol)
+
+user.addr = 'Seoul';   // ⇐⇒ user = {...user, addr: 'Seoul'}
+console.log('addr' in user, user.hasOwnProperty('addr')); // true true
+console.log('Ref.has>', Reflect.has(user, 'addr'));  // true
+console.log('obj.getOwnPropSym>', Object.getOwnPropertySymbols(user)); // [ Symbol() ]
+
+
+delete user.addr;    // ⇔ Reflect.deleteProperty(user, 'addr');
+console.log('addr' in user); // false
+
+
+user[`${user.id}'s name`] = `Mr. ${user.name}`;     // prop생성시 snapshot!!(id 변해도 고정)
+console.log('user entries=', Object.entries(user)); // Symbol은 제외!!
+
+function myEntries(obj) {
+    const rets = []; //[[k,v],[k,v],...]
+    for (let k of Reflect.ownKeys(obj)) {
         console.log(k);
-        rets.push([k,obj[k]]);
+        rets.push([k, obj[k]]);
     }
     return rets;
-  }
-  console.log(myEntries(user));
-  console.log("-----------------");
+}
+console.log(myEntries(user));
+console.log("-----------------");
 
 console.log(Object.getOwnPropertyDescriptor(user, 'id'));  // value, writable,
 console.log(Object.getOwnPropertyDescriptors(user));  // enumerable(속성 노출 여부), configurable
 //enumerable=false
-Object.defineProperty(user, 'age', { value: 39, writable: false});
+Object.defineProperty(user, 'age', { value: 39, writable: false });
 // age는 writable, enumerable, configurable 모두 false ⇒ Object.keys()에서 제외!
 // Object.keys(user) vs Object.values(user) vs Object.entries(user)
-console.log(Object.getOwnPropertyDescriptors(user));  
-const park = Object.fromEntries([ ['id', 7], ['nm', 'Park'] ]);
-const emp=Object.assign({'id':100}, user);
+console.log(Object.getOwnPropertyDescriptors(user));
+const park = Object.fromEntries([['id', 7], ['nm', 'Park']]);
+const emp = Object.assign({ 'id': 100 }, user);
 //같은 키가 있을땐 덮어씌움, 뒤가 우선하니까 
 console.log(emp);
-console.log({'id':100},{...user});
+console.log({ 'id': 100 }, { ...user });
 //  vs  new Object(user) vs  Object.create(user)    ⇒ ⇒ ⇒
 // Object의 생성자함수에 매개변수에 object를 주면 그대로 반환!!
 Object.preventExtensions(user); // 추가, 삭제, 읽기, 쓰기, 재정의
@@ -72,56 +72,123 @@ const fibonacci = memoized(function A(n) {
     if (n === 2) return 1;
     if (n === 3) return 2;
     return fibonacci(n - 2) + fibonacci(n - 1);
-    });
+});
 
 
-  function memoized(fn) { // 범용 memoization function
+function memoized(fn) { // 범용 memoization function
     const memoizedTable = {}; // {3: 3 * 2, 2: 2 * 1, 1: 1}
     return function B(k) {
         console.log(memoizedTable);
-      return memoizedTable[k] || (memoizedTable[k] = fn(k));
+        return memoizedTable[k] || (memoizedTable[k] = fn(k));
     };
-  }
-  
-  console.log(fibonacci(5)); //5
-  console.log(fibonacci(7));//13
+}
+
+console.log(fibonacci(5)); //5
+console.log(fibonacci(7));//13
 
 
 const arr = [100, 200, 300, 400, 500, 600, 700];
 // 배열의 인덱스 출력 1
 for (const index in arr) {
-  console.log('Index:', index);
+    console.log('Index:', index);
 }
 
 // 배열의 값 출력 2
 for (const value of arr) {
-  console.log('Value:', value);
+    console.log('Value:', value);
 }
 
 const obj = { name: 'lim', addr: 'Yongsan', level: 1, role: 9, receive: false };
- 
+
 // 3. for-in문을 사용하여 프로퍼티 이름(키)을 출력하시오.
 for (const k in obj) {
     console.log('objkey:', k);
-  }
+}
 // 4. for-in문을 사용하여 프로퍼티 값을 출력하시오.
 for (const k in obj) {
-    console.log('objkey:',obj[k]);
-  }
+    console.log('objkey:', obj[k]);
+}
 
 // 5. for-of문을 사용하여 프로퍼티 값을 출력하시오.
-function myEntries(obj){
-    for (let k of Reflect.ownKeys(obj)){
+function myEntries(obj) {
+    for (let k of Reflect.ownKeys(obj)) {
         console.log(obj[k]);
     }
-  }
+}
 myEntries(obj);
+
+for (const k in Object.keys(obj)) {
+    console.log('objkey:', k);
+}
 
 // 6. level 프로퍼티가 열거(entries)되지 않도록 설정하시오.
 //  // Object.defineProperty
-
-
+console.log("----------------------")
+console.log("전", Object.entries(obj));
+Object.defineProperty(obj, 'level', { enumerable: false });
+console.log("후", Object.entries(obj));
+console.log("----------------------")
 // 7. role 프로퍼티는 읽기전용으로 설정하시오. // Object.freeze
+console.log(Object.entries(obj));
+Object.defineProperty(obj, 'role', { writable: false });
+obj.role = 'XXXXXXXX';
+console.log(Object.entries(obj));
+
+console.log("----------------------")
+//[['A', 10, 20], ['B', 30, 40], ['C', 50, 60, 70]] 배열을 객체로 만드시오. (makeObjectFromArray)
+// ex1();
+
+const orgArray = [
+    ['A', 10, 20],
+    ['B', 30, 40],
+    ['C', 50, 60, 70],
+];
+function makeObjectFromArray(arr) {
+    const retObj = {};
+    for (const [k, ...v] of arr) {
+        console.log(k, "ddd", v)
+        retObj[k] = v;
+    }
+    return retObj;
+}
+function makeArrayFromObject(obj) {
+    const retArr = [];
+    for (const k in obj) {
+        console.log(k);
+        retArr.push([k, ...obj[k]]);
+    }
+    return retArr;
+}
+//[['A', 10, 20], ['B', 30, 40], ['C', 50, 60, 70]] 배열을 객체로 만드시오. (makeObjectFromArray)
+
+const newObj = makeObjectFromArray(orgArray);
+console.log('🚀  newObj:', newObj);
+
+console.log("----------------------")
+//위에서 만든 객체를 다시 배열로 만드시오. (makeArrayFromObject)
+const newArr = makeArrayFromObject(newObj);
+console.log('🚀  newArr:', newArr);
 
 
+console.log("----------------------")
+//원시값(primitive)만을 갖는 객체 kim을 복사하는 프로그램을 Object의 클래스 메소드 또는 spread(...) 연산자를 사용하지 말고 작성하시오.
 
+function ex3() {
+    const kim = { nid: 3, nm: 'Hong', addr: 'Pusan' };
+
+    function copyObject(obj) {
+        const retObj = {};
+        for (const k in obj) {
+            console.log("----", k)
+            retObj[k] = obj[k];
+        }
+        return retObj;
+    }
+
+    const newKim = copyObject(kim);
+    console.log('🚀  copiedKim:', newKim);
+    newKim.addr = 'Daegu';
+    console.log(kim.addr, newKim.addr); //바뀜
+    console.log(kim.addr !== newKim.addr); // true면 통과!
+}
+ex3();
